053aa6a53f03321938209124d5f50b278e66cf28
Jon Connolly <jontconnolly@gmail.com>
2016-04-11 16:27:18 -0700

    modify script and Dockerfile

--- 7.8/entrypoint.sh
+++ 7.8/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#this doesn't really work since eworm spins up other processes which end up zombies. 
 set -e
 export EW_LOG=/eworm/run/log/
 export EW_PARAMS=/eworm/run/params/
--- ew_containerize.sh
+++ ew_containerize.sh
@@ -4,5 +4,6 @@
 #docker start eworm
 #and connect with 
 #docker exec -it eworm /bin/bash
-dir=$(dirname $0)
-docker run -it --name eworm -v $dir/params:/eworm/run/params joncon/eworm:7.8 startstop
+dir=$(pwd)
+# docker run -it --name eworm -v $dir/params:/eworm/run/params joncon/eworm:7.8 startstop
+docker run -it --name eworm --rm -v $dir/params:/eworm/run/params joncon/eworm:7.8 /bin/bash
