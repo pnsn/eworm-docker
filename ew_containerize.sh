aae9befd466a996eb1228b0c2d2bee52440574fd
Jon Connolly <jontconnolly@gmail.com>
2016-04-22 15:18:14 -0700

    add 64 bit build

--- ew_containerize.sh
+++ ew_containerize.sh
@@ -6,4 +6,4 @@
 #docker exec -it eworm /bin/bash
 dir=$(pwd)
 # docker run -it --name eworm -v $dir/params:/eworm/run/params joncon/eworm:7.8 startstop
-docker run -it --name eworm --rm -v $dir/params:/eworm/run/params joncon/eworm:7.8 /bin/bash
+docker run -it --name eworm --rm -v $dir/params:/eworm/run/params joncon/eworm-docker:0.1 /bin/bash
